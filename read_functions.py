398cc62ffcf5aba4931eb820d1ae7895a759b3bc
KristofferFJ <kristofferfage@gmail.com>
2021-12-02 08:25:21 +0100

    [day2] done

--- read_functions.py
+++ read_functions.py
@@ -2,3 +2,9 @@ def read_as_ints(filename):
     f = open(filename, "r")
     string_list = f.read().split("\n")
     return [int(i) for i in string_list]
+
+
+def read_as_string_pairs(filename):
+    f = open(filename, "r")
+    string_list = f.read().split("\n")
+    return [i.split(" ") for i in string_list]
